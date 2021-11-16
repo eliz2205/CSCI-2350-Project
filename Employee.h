fe3960b18ae3bc506994f05ffa06f3b3245b5f7a
Eliz2205 <elizabethbeckeris@gmail.com>
2021-11-16 13:21:31 +0000

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,16 +1,24 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 class Employee
 {
-    public:
-        Employee();
-        virtual ~Employee();
+private:
+    int id;
+    string name;
+    string payType;
+    double payAmount;
 
-    protected:
-
-    private:
+public:
+    virtual void calculatePayroll() = 0;
+    
+    void printPaycheck() { cout << payAmount << endl; }
+    
 };
 
-#endif // EMPLOYEE_H
+#endif
