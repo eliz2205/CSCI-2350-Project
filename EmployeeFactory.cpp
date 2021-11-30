d5ffe32bc47caae21db03f83e009ed1e4e1229b9
Eliz2205 <elizabethbeckeris@gmail.com>
2021-11-30 08:13:47 -0500

    Update EmployeeFactory.cpp

--- EmployeeFactory.cpp
+++ EmployeeFactory.cpp
@@ -1,39 +1,74 @@
+#include "Employee.h"
+#include "EmployeeFactory.cpp"
 #include <string>
-​
+#include <iostream>
+#include "Hourly.h"
+#include "Piecework.h"
+#include "Commission.h"
+#include "Salary.h"
+
 using namespace std;
-​
-Employee* buildEmployee(string str)
+
+static Employee* EmployeeFactory::buildEmployee(string str, int date)
 {
-​
-	string rem = str.substring(str.find(" "));
-	int id = 0; 
-	// first space to second space = id
-	rem >> id;
-​
-	rem = rem.substring(rem.find(" "));
-	int payAmount = 0;
-	// second space to the third space = payAmount
-	rem >> payAmount;
-​
-	rem = rem.substring(rem.find(" "));
-	string name = rem;
-	
-	if(str.find("salary") != string::npos)
-	{
-		return new Salary(id, payAmount, name);
-	}
-	else if(str.find("hourly") != string::npos)
-	{
-		return new Hourly(id, payAmount, name);
-	}
-	else if(str.find("piecework") != string::npos)
-	{
-		return new Piecework(id, payAmount, name);
-	}
-	else if(str.find("commission") != string::npos)
-	{
-		return new Commision(id, payAmount, name);
-	}
-	
-	throw invalid_argument;
+    // comma delimited
+    // line format: TYPE,id,payRate,payAmount, terminated,[numDaysWorked],name
+
+    // Declare dfields
+    int id;
+    string empType;
+    double payRate;
+    string name;
+    string remainder;
+    int daysWorked = 0;
+
+    // get EmpType
+    empType = str.substr(0, remainder.find(","));
+    remainder = remainder.substr(remainder.find(",") + 1);
+    
+    // get ID
+    id = stoi(remainder.substr(0, str.find(",")));
+    remainder = remainder.substr(str.find(",") + 1);
+    
+    // get payrate
+    payRate = stod(remainder.substr(0, remainder.find(",")));
+    remainder = remainder.substr(remainder.find(",") + 1);
+
+    // skip payAmount
+    remainder = remainder.substr(remainder.find(",") + 1);
+    
+    // skip terminated
+    remainder = remainder.substr(remainder.find(",") + 1);
+
+    // get numdaysworked if hourly
+    if (empType.compare("hourly"))
+    {
+        daysWorked = stoi(remainder.substr(0, remainder.find(",")));
+        remainder = remainder.substr(remainder.find(",") + 1);
+    }
+    
+    // get name
+    name = remainder;
+
+    if (empType.compare("hourly"))
+    {
+        //type, id, payRate, name, lastDayWOrked, numDaysWorked
+        return new Hourly(empType, id, payRate, name, date, daysWorked);
+    }
+
+    else if (empType.compare("piecework"))
+    {
+        return new Piecework(empType, id, payRate, name);
+    }
+
+    else if (empType.compare("commission"))
+    {
+        return new Commission(empType, id, payRate, name);
+    }
+
+    else if (empType.compare("salary"))
+    {
+        // string employeeType, int id, double payRate, string name, int firstDay, int lastDay
+        return new Salary(empType, id, payRate, name, date, -1);
+    }
 }
