4df1575680f601201e870a753cebc0353bb5400f
Eliz2205 <elizabethbeckeris@gmail.com>
2021-11-30 17:28:23 -0500

    Update EmployeeFactory.cpp

--- EmployeeFactory.cpp
+++ EmployeeFactory.cpp
@@ -1,19 +1,24 @@
+// RUNTIME ERROR
+
 #include "Employee.h"
-#include "EmployeeFactory.cpp"
+#include "EmployeeFactory.h"
 #include <string>
 #include <iostream>
 #include "Hourly.h"
 #include "Piecework.h"
-#include "Commission.h"
 #include "Salary.h"
 
 using namespace std;
 
+// todo : if time, make less repetetive
 
-static Employee* EmployeeFactory::buildEmployee(string str, int date)
+// todo : switch order to match his order
+
+Employee* EmployeeFactory::buildEmployee(string str, int date)
 {
     // comma delimited
-    // line format: TYPE,id,payRate,payAmount, terminated,[numDaysWorked],name
+    // line format: type,id,payRate,payAmount,name
+    // todo : add DailyTransaction files as dependencies in makefile
 
     // Declare dfields
     int id;
@@ -21,74 +26,76 @@ static Employee* EmployeeFactory::buildEmployee(string str, int date)
     double payRate;
     string name;
     string remainder;
-    int daysWorked = 0;
+
+    // todo : make less repetitive ? 
 
     // get EmpType
-    empType = str.substr(0, remainder.find(","));
-    remainder = remainder.substr(remainder.find(",") + 1);
-    
+    empType = str.substr(0, str.find(","));
+    remainder = str.substr(str.find(",") + 1);
+ 
     // get ID
     id = stoi(remainder.substr(0, str.find(",")));
-    remainder = remainder.substr(str.find(",") + 1);
+    remainder = remainder.substr(remainder.find(",") + 1);
+    
+    cout << "ID: " << id << endl;
     
     // get payrate
     payRate = stod(remainder.substr(0, remainder.find(",")));
     remainder = remainder.substr(remainder.find(",") + 1);
-
-    // skip payAmount
-    remainder = remainder.substr(remainder.find(",") + 1);
-    
-    // skip terminated
-    remainder = remainder.substr(remainder.find(",") + 1);
-
-    // get numdaysworked if hourly
-    if (empType.compare("hourly"))
-    {
-        daysWorked = stoi(remainder.substr(0, remainder.find(",")));
-        remainder = remainder.substr(remainder.find(",") + 1);
-    }
     
     // get name
+    
     name = remainder;
 
-    if (empType.compare("hourly"))
+    if (empType == "hourly")
     {
-        //type, id, payRate, name, lastDayWOrked, numDaysWorked
-        return new Hourly(empType, id, payRate, name, date, daysWorked);
+        //type, id, payRate, name, lastDayWOrked, numConsecutive
+        return new Hourly(empType, id, payRate, name, date, 1);
     }
 
-    else if (empType.compare("piecework"))
+    else if (empType == "piecework")
     {
+        // string employeeType, int id, double payRate, string name
         return new Piecework(empType, id, payRate, name);
     }
 
-    else if (empType.compare("commission"))
-    {
-        return new Commission(empType, id, payRate, name);
-    }
-
-    else if (empType.compare("salary"))
+    else if (empType == "salary")
     {
         // string employeeType, int id, double payRate, string name, int firstDay, int lastDay
         return new Salary(empType, id, payRate, name, date, -1);
     }
+
+    else if (empType == "commission")
+    {
+        // todo : write commission and return new commission employee
+        return nullptr;
+    }
+
+    else
+    {
+        return nullptr;
+    }
 }
-static Employee* EmployeeFactory::insertEmployee(string str)
+
+
+
+Employee* EmployeeFactory::insertEmployee(string str)
 {
-// space delimited
-    // line format: TYPE name payRate payAmount terminated [numDaysWorked] id
+    // space delimited
+    // line format: TYPE id payRate payAmount terminated [numDaysWOrked] name
 
     // Declare dfields
-    int id;
+    int id, terminated, date;
     string empType;
     double payRate;
+    double payAmount;
     string name;
     string remainder;
-    int daysWorked = 0;
+    int numConsecutive = -1;
 
     // get EmpType
-    empType = str.substr(0, remainder.find(" "));
-    remainder = remainder.substr(remainder.find(" ") + 1);
+    empType = str.substr(0, str.find(" "));
+    remainder = str.substr(str.find(" ") + 1);
     
     // get id
     id = stoi(remainder.substr(0, str.find(" ")));
@@ -98,42 +105,47 @@ static Employee* EmployeeFactory::insertEmployee(string str)
     payRate = stod(remainder.substr(0, remainder.find(" ")));
     remainder = remainder.substr(remainder.find(" ") + 1);
 
-    // skip payAmount
+    // get payAmount
+    payAmount = stod(remainder.substr(0, remainder.find(" ")));
     remainder = remainder.substr(remainder.find(" ") + 1);
     
-    // skip terminated
+    // get terminated
+    terminated = stoi(remainder.substr(0, remainder.find(" ")));
     remainder = remainder.substr(remainder.find(" ") + 1);
 
-    if (empType.compare("hourly"))
+    if (empType == "hourly")
     {
-        daysWorked = stoi(remainder.substr(0, remainder.find(",")));
-        remainder = remainder.substr(remainder.find(",") + 1);
+        numConsecutive = stoi(remainder.substr(0, remainder.find(" ")));
+        remainder = remainder.substr(remainder.find(" ") + 1);
     }
     
     name = remainder;
     
-    if (empType.compare("hourly"))
+    if (empType == "hourly")
     {
-        //type, id, payRate, name, lastDayWOrked, numDaysWorked
-        return new Hourly(empType, id, payRate, name, date, daysWorked);
+       //type, id, payRate, name, lastDayWOrked, numConsecutive
+        return new Hourly(empType, id, payRate, name, -1, numConsecutive);
     }
 
-    else if (empType.compare("piecework"))
+    else if (empType == "piecework")
     {
+        // string employeeType, int id, double payRate, string name
         return new Piecework(empType, id, payRate, name);
     }
 
-    else if (empType.compare("commission"))
+    else if (empType == "commission")
     {
-        return new Commission(empType, id, payRate, name);
+        return nullptr;
     }
 
-    else if (empType.compare("salary"))
+    else if (empType == "salary")
     {
         // string employeeType, int id, double payRate, string name, int firstDay, int lastDay
         return new Salary(empType, id, payRate, name, date, -1);
     }
 
 }
+
+
 
 
